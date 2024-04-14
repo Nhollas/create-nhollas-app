f0a163d98ffff8c44ca7d844f73b05b52842fc8c
Nick Hollas <nicholashollas@yahoo.co.uk>
2024-04-14 11:02:37 +0100

    Update example schema to have max char validation.

--- apps/cli/templates/default/app/features/example/schemas.ts
+++ apps/cli/templates/default/app/features/example/schemas.ts
@@ -3,7 +3,15 @@ import { z } from "zod"
 export const exampleSchema = z
   .object({
     id: z.string().uuid({ message: "Id must be a valid UUID." }),
-    title: z.string().min(1, { message: "Title cannot be empty." }),
-    description: z.string().min(1, { message: "Description cannot be empty." }),
+    title: z
+      .string()
+      .min(1, { message: "Title cannot be empty." })
+      .max(256, { message: "Title cannot be more than 256 characters." }),
+    description: z
+      .string()
+      .min(1, { message: "Description cannot be empty." })
+      .max(2048, {
+        message: "Description cannot be more than 2048 characters.",
+      }),
   })
   .strict()
