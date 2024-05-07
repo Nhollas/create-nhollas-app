3383e0caf64a02e49e49031b65cd1241d3e1ace4
Nick Hollas <nicholashollas@yahoo.co.uk>
2024-05-07 22:47:25 +0100

    Remove console logs.

--- apps/cli/helpers/copy.ts
+++ apps/cli/helpers/copy.ts
@@ -1,24 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { async as glob } from "fast-glob";
-import path from "path";
-import fs from "fs";
+import { async as glob } from "fast-glob"
+import path from "path"
+import fs from "fs"
 
 interface CopyOption {
-  cwd?: string;
-  rename?: (basename: string) => string;
+  cwd?: string
+  rename?: (basename: string) => string
 }
 
-const identity = (x: string) => x;
+const identity = (x: string) => x
 
 export const copy = async (
   src: string | string[],
   dest: string,
-  { cwd, rename = identity }: CopyOption = {}
+  { cwd, rename = identity }: CopyOption = {},
 ) => {
-  const source = typeof src === "string" ? [src] : src;
+  const source = typeof src === "string" ? [src] : src
 
   if (source.length === 0 || !dest) {
-    throw new TypeError("`src` and `dest` are required");
+    throw new TypeError("`src` and `dest` are required")
   }
 
   const sourceFiles = await glob(source, {
@@ -26,54 +26,38 @@ export const copy = async (
     dot: true,
     absolute: false,
     stats: false,
-  });
+  })
 
-  console.log("Source files", sourceFiles);
+  const destRelativeToCwd = cwd ? path.resolve(cwd, dest) : dest
 
-  const destRelativeToCwd = cwd ? path.resolve(cwd, dest) : dest;
-
-  console.log("Source files", sourceFiles);
-
-  const prefixDotWhitelist = ["vscode"];
+  const prefixDotWhitelist = ["vscode"]
 
   const prefixFolderWithDot = (
     dirname: string,
     basename: string,
-    destRelativeToCwd: string
+    destRelativeToCwd: string,
   ) => {
-    return path.join(destRelativeToCwd, "." + dirname, basename);
-  };
+    return path.join(destRelativeToCwd, "." + dirname, basename)
+  }
 
   return Promise.all(
     sourceFiles.map(async (p) => {
-      const dirname = path.dirname(p);
+      const dirname = path.dirname(p)
 
-      console.log("Dirname", dirname);
+      const basename = rename(path.basename(p))
 
-      const basename = rename(path.basename(p));
+      const from = cwd ? path.resolve(cwd, p) : p
 
-      console.log("Basename", basename);
-
-      const from = cwd ? path.resolve(cwd, p) : p;
-
-      console.log("From", from);
-      let to = path.join(destRelativeToCwd, dirname, basename);
-
-      console.log("To", to);
+      let to = path.join(destRelativeToCwd, dirname, basename)
 
       if (prefixDotWhitelist.includes(dirname)) {
-        console.log(
-          "prefixFolderWithDot",
-          prefixFolderWithDot(dirname, basename, destRelativeToCwd)
-        );
-
-        to = prefixFolderWithDot(dirname, basename, destRelativeToCwd);
+        to = prefixFolderWithDot(dirname, basename, destRelativeToCwd)
       }
 
       // Ensure the destination directory exists
-      await fs.promises.mkdir(path.dirname(to), { recursive: true });
+      await fs.promises.mkdir(path.dirname(to), { recursive: true })
 
-      return fs.promises.copyFile(from, to);
-    })
-  );
-};
+      return fs.promises.copyFile(from, to)
+    }),
+  )
+}
